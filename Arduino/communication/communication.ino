04d894780e142a2a23e26045ef817cf5d870465a
ericy <ericy@stud.ntnu.no>
2019-09-28 00:18:21 +0200

    arduino read improved

---
+++ Arduino/communication/communication.ino
@@ -0,0 +1,62 @@
+class MotorControl {
+  public:
+    void goForward() {
+      Serial.print("Forward");
+    }
+
+    void reverse() {
+      Serial.print("Reverse");
+    }
+
+    void turnLeft() {
+      Serial.print("Turn left");
+    }
+
+    void turnRight() {
+      Serial.print("Turn right");
+    }
+
+    void pivotLeft() {
+      Serial.print("Pivot left");
+    }
+
+    void pivotRight() {
+      Serial.print("Pivot right");
+    }
+
+    void stopMotor() {
+      Serial.print("Stop motor");
+    }
+};
+
+
+
+String message = "";
+MotorControl driver;
+
+void setup() {
+  driver = MotorControl();
+  Serial.begin(9600);
+  Serial.write("Connected");
+}
+
+
+void loop() {
+
+  while (Serial.available() > 0) {
+    byte received = Serial.read();
+    message += char(received);
+  }
+
+  if (message != "") {
+    messageHandler(message);
+    message = ""; 
+  }
+}
+
+void messageHandler(String message){
+  if(message == "forward"){
+    driver.goForward();
+  }
+  driver.goForward();
+}
