f19ce49971960d88186335a4d83012f7dc223e12
ericy <ericy@stud.ntnu.no>
2019-10-16 19:47:26 +0200

    update

--- Arduino/communication/communication.ino
+++ Arduino/communication/communication.ino
@@ -1,41 +1,57 @@
+int ledPin = 13;
 
-//char buf[200] = {};
 
 void setup() {
   Serial.begin(9600);
-  Serial.write("Connected\n");
+  Serial.write("Connected");
+  pinMode(ledPin, OUTPUT);
 }
 
 
 void loop() {
-  incomingMessage();
+  String incomingMsg = "";
+  
+  if(Serial.available() >0){
+    incomingMsg = incomingMessage(); 
+  }
+
+  if(incomingMsg != ""){
+    handleMessage(incomingMsg);
+  }
   //outgoingMessage();
+  blinkLed();
 }
 
-void incomingMessage(){
-  //int readForN = Serial.read();
-  int incomingByte;
-  incomingByte = Serial.read();
 
+void blinkLed(){
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
+
+
+String incomingMessage(){
+  int incomingByte = Serial.read();
+  String message = "";
+  
     if(incomingByte != -1){
       char buf[200] = {};
       Serial.readBytes(buf, incomingByte);
-
-      String message = "";
-      
       for(int i = 0; i <= incomingByte; i++){
           message += buf[i];
       }
-      
-      //Serial.print(message);
-      handleMessage(message);
     }
+    return message;
 }
 
 void handleMessage(String message){
-     outgoingMessage("Arduino got: " + message);
+     outgoingMessage(message);
 }
 
 void outgoingMessage(String message){
-  Serial.print(message);
+  for(int i=0; i<message.length(); i++){
+    Serial.write(message[i]);
+  }
+
 }
